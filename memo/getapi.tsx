f66e19fda2bd91fcffcf326fbc3796b3e6fc366d
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2023-12-28 07:05:29 +0900

    getapi.tsx の更新

--- memo/getapi.tsx
+++ memo/getapi.tsx
@@ -28,33 +28,3 @@ export default function Youtube() {
     console.log(channelData)
 
 }
-
-
-
-  return (
-    <body>
-
-      <header>
-        <div className='h-28 bg-red-600 flex items-center justify-center'>
-          <div className='w-9/12'>
-            <h1 className='text-4xl text-white pt-4'>けんぴ。ちゃんテナ</h1>
-            <p className='pt-2 pl-2'>from Youtube</p>
-          </div>
-        </div>
-      </header>
-
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className='grid sm:grid-cols-2 my-8 mx-4 py-4 px-4 border-solid border-gray-400 border-2 rounded'>
-          <div className='my-4 mx-4'>
-
-          </div>
-          <div className='flex items-center justify-center'>
-
-          </div>
-        </div>
-      </main>
-
-    </body>
-
-  )
-}
