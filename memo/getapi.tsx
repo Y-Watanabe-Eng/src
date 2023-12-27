b13d97170365b99b85d769a322d27a78f641fb6c
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2023-12-28 08:08:23 +0900

    getapi.tsx の更新

--- memo/getapi.tsx
+++ memo/getapi.tsx
@@ -8,17 +8,18 @@ interface Props {
 export default function getYoutube() {
 
   const apiKey = process.env.YOUTUBE_API_KEY
-  const channelID = 'UCUBvFfyuBrATTPxnAZ4OsCQ'
+  const channelID = "UCUBvFfyuBrATTPxnAZ4OsCQ"
+  const playlistID = ""
 
   async function getChannel() {
     'use server'
 
     const channelRes = await fetch(
-      'https://www.googleapis.com/youtube/v3/channels?part=' +
-        'snippet' +
-        '&id=' +
+      "https://www.googleapis.com/youtube/v3/channels?part=" +
+        "snippet" +
+        "&id=" +
         channelID + 
-        '&key=' +
+        "&key=" +
         apiKey
     )}
 
