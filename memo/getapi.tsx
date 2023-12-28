4c8bef92b40ecb47beb534997b8f7101e6d75faa
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2023-12-28 18:26:28 +0900

    getapi.tsx の更新

--- memo/getapi.tsx
+++ memo/getapi.tsx
@@ -1,16 +1,17 @@
 async function getYoutube() {
 
   const apiKey = process.env.YOUTUBE_API_KEY
-  const channelID = "UCUBvFfyuBrATTPxnAZ4OsCQ"
+  const channelId = "UCUBvFfyuBrATTPxnAZ4OsCQ"
 
+チャンネル情報の取得
   async function getChannel() {
 //    'use server'
 
     const channelRes = await fetch(
       "https://www.googleapis.com/youtube/v3/channels?part=" +
-        "snippet" +
+        "contentDetails" +
         "&id=" +
-        channelID + 
+        channelId + 
         "&key=" +
         apiKey
       )
@@ -25,6 +26,43 @@ async function getYoutube() {
 
   console.log(channelData)
 
+  const uploadsId = channelData.items[0].contentDetails.relatedPlaylists.uploads
+  
+  console.log(uploadsId)
+
+  
+//プレイリスト情報の取得
+  async function getPlaylist() {
+//    'use server'
+
+    const playlistRes = await fetch(
+      "https://www.googleapis.com/youtube/v3/playlistItems?part=" +
+        "contentDetails" +
+        "&playlistId=" +
+        uploadsId +
+        "&maxResults=50" +
+        "&key=" +
+        apiKey
+      )
+      
+    const playlistData = await playlistRes.json()
+
+    return playlistData;
+
+  }
+
+  const playlistData = await getPlaylist()
+
+  console.log(playlistData)
+
+  const videoIdArray = []
+
+  for (let i = 0; i < 50; i++) {
+    videoIdArray.push(playlistData.items[i].contentDetails.videoId)
+  }
+  
+  console.log(videoIdArray)
+  
 }
 
 getYoutube()
